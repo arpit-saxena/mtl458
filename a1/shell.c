5ae4c385b4c2c854c3598bb5195110b9c3f031c7
Arpit Saxena <arpit.saxena2000@yahoo.in>
2021-02-27 00:46:22 +0530

    Add string input function

--- a1/shell.c
+++ a1/shell.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
+// Safe arbitrary length string input function taken from
+// https://stackoverflow.com/a/16871702/5585431
+const int init_str_size = 20;
+char *input_str() {
+  char ch;
+  size_t len = 0;
+  int size = init_str_size;
+
+  char *str = malloc(sizeof(*str) * size);
+  if (!str) { // Error. Return null. Caller handles error
+    return str;
+  }
+
+  while (EOF != (ch = fgetc(stdin)) && ch != '\n') {
+    str[len++] = ch;
+    if (len == size) {
+      str = realloc(str, sizeof(*str) * (size += 16));
+      if (!str)
+        return str;
+    }
+  }
+  str[len++] = '\0';
+
+  return str;
+}
+
 int main(void) {
-  printf("Hello, world!\n");
+  printf("What's your name? ");
+  char *const name = input_str();
+  printf("Hi %s\n", name);
+  free(name);
   return 0;
 }
