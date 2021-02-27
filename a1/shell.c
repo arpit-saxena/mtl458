d7c12e0000d342313414751bd43268d832c835ce
Arpit Saxena <arpit.saxena2000@yahoo.in>
2021-02-27 11:24:36 +0530

    Change ~ to mean directory in which shell script started
    As specified in answer to @15 on Piazza

--- a1/shell.c
+++ a1/shell.c
@@ -25,24 +25,22 @@ char *curr_dir = NULL;
 // Current directory to print, containing appropriate transformations such as
 // changing HOME to ~
 char *curr_print_dir = NULL;
+char *start_dir = NULL;
 
 char *get_print_dir(char *dir) {
   char *print_dir = strdup(dir);
 
-  char *const home_dir = getenv("HOME");
-  if (home_dir) {
-    int home_dir_len = strlen(home_dir);
-    int match = strncmp(home_dir, dir, home_dir_len) == 0;
+  int start_dir_len = strlen(start_dir);
+  int match = strncmp(start_dir, dir, start_dir_len) == 0;
 
-    if (match) {
-      int print_dir_len = strlen(dir) - strlen(home_dir) + 1;
-      free(print_dir); // We had dup'd dir earlier
-      print_dir = (char *)malloc(sizeof(char *) * print_dir_len);
-      print_dir[0] = '\0';
+  if (match) {
+    int print_dir_len = strlen(dir) - strlen(start_dir) + 1;
+    free(print_dir); // We had dup'd dir earlier
+    print_dir = (char *)malloc(sizeof(char *) * print_dir_len);
+    print_dir[0] = '\0';
 
-      strcat(print_dir, "~");
-      strcat(print_dir, dir + home_dir_len);
-    }
+    strcat(print_dir, "~");
+    strcat(print_dir, dir + start_dir_len);
   }
 
   return print_dir;
@@ -50,6 +48,7 @@ char *get_print_dir(char *dir) {
 
 void init() {
   curr_dir = getcwd(NULL, 0);
+  start_dir = strdup(curr_dir);
   curr_print_dir = get_print_dir(curr_dir);
 }
 
