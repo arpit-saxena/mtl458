2d0c5a32d33e6e539040c4ddd9ae1049bfed7f77
Arpit Saxena <arpit.saxena2000@yahoo.in>
2021-03-07 21:22:44 +0530

    Move input_str() into shell.c

--- a1/shell.c
+++ a1/shell.c
@@ -13,6 +13,7 @@ void init();
 void cleanup();
 void sigint_handler(int sig_no);
 char *get_cmd();
+char *input_str();
 
 char *const cd_cmd = "cd";
 char *const history_cmd = "history";
@@ -251,4 +252,36 @@ char *get_cmd() {
   }
 
   return cmd;
+}
+
+// Safe arbitrary length string input function taken from
+// https://stackoverflow.com/a/16871702/5585431
+extern const int init_str_size;
+extern int got_eof;
+char *input_str() {
+  char ch;
+  size_t len = 0;
+  int size = init_str_size;
+
+  char *str = malloc(sizeof(*str) * size);
+  if (!str) { // Error. Return null. Caller handles error
+    return str;
+  }
+
+  while (EOF != (ch = fgetc(stdin)) && ch != '\n') {
+    str[len++] = ch;
+    if (len == size) {
+      str = realloc(str, sizeof(*str) * (size += 16));
+      if (!str)
+        return str;
+    }
+  }
+
+  if (ch == EOF) {
+    got_eof = 1;
+  }
+
+  str[len++] = '\0';
+
+  return str;
 }
