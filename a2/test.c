b099ec05219a9a11d8ef84763a5707f671448433
Arpit Saxena <arpit.saxena2000@yahoo.in>
2021-03-29 22:58:54 +0530

    Add final test.c and update .gitignore

--- a2/test.c
+++ a2/test.c
@@ -48,6 +48,36 @@ void test2() {
   print_mem();
 }
 
+extern free_header_t *next_fh;
+extern free_header_t *prev_fh;
+
+void test3() {
+  void *arr[100];
+  int size = 0;
+  while (arr[size++] = my_alloc(400)) {
+    print_mem();
+  }
+  void *rem = my_alloc(16);
+
+  print_mem();
+
+  for (int i = 0; i < size; i += 2) {
+    my_free(arr[i]);
+  }
+  my_free(arr[1]);
+
+  print_mem();
+
+  my_alloc(8);
+  print_mem();
+
+  my_alloc(1000);
+  print_mem();
+
+  my_alloc(1000);
+  print_mem();
+}
+
 int main(void) {
   if (my_init()) {
     perror("my_init");
@@ -55,6 +85,28 @@ int main(void) {
 
   print_info();
   print_mem();
+
+  void *arr[1000];
+  int size = 0;
+  while (arr[size++] = my_alloc(8)) {
+  }
+  print_mem();
+
+  for (int i = 0; arr[i]; i += 2) {
+    my_free(arr[i]);
+  }
+  print_mem();
+
+  my_free(arr[1]);
+  my_heapinfo();
+
+  my_free(NULL);
+
+  my_alloc(8);
+
+  my_alloc(8);
+  my_alloc(16);
+  my_heapinfo();
 
   my_clean();
 }
