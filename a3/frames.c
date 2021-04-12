be27369efe720891580df8eb4a6bc4235b045a6d
Arpit Saxena <arpit.saxena2000@yahoo.in>
2021-04-12 12:05:15 +0530

    Add command line parsing logic

---
+++ a3/frames.c
@@ -0,0 +1,91 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum strategy_t { OPT, FIFO, CLOCK, LRU, RANDOM };
+
+struct cmdline_args_t {
+  FILE *input_file;
+  int num_frames;
+  enum strategy_t strategy;
+  bool verbose;
+};
+
+struct cmdline_args_t extract_cmdline_args(int argc, char *argv[]) {
+  struct cmdline_args_t args;
+  if (argc == 5) {
+    assert(strcmp(argv[argc - 1], "-verbose") == 0 &&
+           "-verbose should be the last argument passed, if it is present");
+    args.verbose = true;
+  } else {
+    args.verbose = false;
+  }
+
+  if (argc != 4 && argc != 5) {
+    fprintf(stderr, "Usage: ./frames <tracefile> <number of frames> "
+                    "<replacement policy> [-verbose]\n");
+    exit(1);
+  }
+
+  char *filename = argv[1];
+  char *num_frames_str = argv[2];
+  char *strat_str = argv[3];
+
+  args.input_file = fopen(filename, "r");
+  if (!args.input_file) {
+    perror("Opening trace file");
+    exit(1);
+  }
+
+  char *end;
+  long res = strtol(num_frames_str, &end, 10);
+  if (res == LONG_MIN || res == LONG_MAX) {
+    perror("Number of frames");
+    exit(1);
+  }
+  if (res <= 0 || res > 1000) {
+    fprintf(stderr, "The number of pages should be in range [1, 1000]\n");
+    exit(1);
+  }
+  if (end[0] != '\0' || num_frames_str[0] == '\0') {
+    fprintf(stderr, "Invalid number of page frames\n");
+    exit(1);
+  }
+  args.num_frames = res;
+
+  if (strcmp(strat_str, "OPT") == 0) {
+    args.strategy = OPT;
+  } else if (strcmp(strat_str, "FIFO") == 0) {
+    args.strategy = FIFO;
+  } else if (strcmp(strat_str, "CLOCK") == 0) {
+    args.strategy = CLOCK;
+  } else if (strcmp(strat_str, "LRU") == 0) {
+    args.strategy = LRU;
+  } else if (strcmp(strat_str, "RANDOM") == 0) {
+    args.strategy = RANDOM;
+  } else {
+    fprintf(stderr, "Unrecognized page replacement strategy\n");
+    exit(1);
+  }
+
+  return args;
+}
+
+void cleanup(FILE *input_file) {
+  if (fclose(input_file)) {
+    perror("fclose");
+  }
+}
+
+int main(int argc, char *argv[]) {
+  struct cmdline_args_t args = extract_cmdline_args(argc, argv);
+
+  printf("Num frames: %d\n", args.num_frames);
+  printf("Strategy: %d\n", args.strategy);
+  printf("Verbose: %d\n", args.verbose);
+
+  cleanup(args.input_file);
+}
