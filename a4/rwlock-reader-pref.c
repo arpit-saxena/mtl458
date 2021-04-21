b4afa127f7284ce291cd7bbbe2d6d362eb081c50
Arpit Saxena <arpit.saxena2000@yahoo.in>
2021-04-21 16:54:04 +0530

    Change resource_lock to num_readers_lock

--- a4/rwlock-reader-pref.c
+++ a4/rwlock-reader-pref.c
@@ -2,26 +2,26 @@
 
 void InitalizeReadWriteLock(struct read_write_lock *rw) {
   rw->num_readers = 0;
-  sem_init(&rw->resource_lock, 0, 1);
+  sem_init(&rw->num_readers_lock, 0, 1);
   sem_init(&rw->write_lock, 0, 1);
 }
 
 void ReaderLock(struct read_write_lock *rw) {
-  sem_wait(&rw->resource_lock);
+  sem_wait(&rw->num_readers_lock);
   rw->num_readers++;
   if (rw->num_readers == 1) {
     sem_wait(&rw->write_lock);
   }
-  sem_post(&rw->resource_lock);
+  sem_post(&rw->num_readers_lock);
 }
 
 void ReaderUnlock(struct read_write_lock *rw) {
-  sem_wait(&rw->resource_lock);
+  sem_wait(&rw->num_readers_lock);
   rw->num_readers--;
   if (rw->num_readers == 0) {
     sem_post(&rw->write_lock);
   }
-  sem_post(&rw->resource_lock);
+  sem_post(&rw->num_readers_lock);
 }
 
 void WriterLock(struct read_write_lock *rw) { sem_wait(&rw->write_lock); }
--- a4/rwlock.h
+++ a4/rwlock.h
@@ -5,9 +5,9 @@
 #include <unistd.h>
 
 struct read_write_lock {
-  sem_t resource_lock; // lock for atomically updating resources of this struct
-  sem_t write_lock;    // lock which writers must have to write
-  int num_readers;     // Number of readers currently reading the resource
+  sem_t num_readers_lock; // lock for num_readers
+  sem_t write_lock;       // lock which writers must have to write
+  int num_readers;        // Number of readers currently reading the resource
 };
 
 void InitalizeReadWriteLock(struct read_write_lock *rw);
