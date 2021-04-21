abb2ccddbe08293bcb3d9ee0a06b5e71f980d21f
Arpit Saxena <arpit.saxena2000@yahoo.in>
2021-04-21 15:58:55 +0530

    Add reader preference lock

--- a4/rwlock-reader-pref.c
+++ a4/rwlock-reader-pref.c
@@ -1,21 +1,29 @@
 #include "rwlock.h"
 
 void InitalizeReadWriteLock(struct read_write_lock *rw) {
-  //	Write the code for initializing your read-write lock.
+  rw->num_readers = 0;
+  sem_init(&rw->resource_lock, 0, 1);
+  sem_init(&rw->write_lock, 0, 1);
 }
 
 void ReaderLock(struct read_write_lock *rw) {
-  //	Write the code for aquiring read-write lock by the reader.
+  sem_wait(&rw->resource_lock);
+  rw->num_readers++;
+  if (rw->num_readers == 1) {
+    sem_wait(&rw->write_lock);
+  }
+  sem_post(&rw->resource_lock);
 }
 
 void ReaderUnlock(struct read_write_lock *rw) {
-  //	Write the code for releasing read-write lock by the reader.
+  sem_wait(&rw->resource_lock);
+  rw->num_readers--;
+  if (rw->num_readers == 0) {
+    sem_post(&rw->write_lock);
+  }
+  sem_post(&rw->resource_lock);
 }
 
-void WriterLock(struct read_write_lock *rw) {
-  //	Write the code for aquiring read-write lock by the writer.
-}
+void WriterLock(struct read_write_lock *rw) { sem_wait(&rw->write_lock); }
 
-void WriterUnlock(struct read_write_lock *rw) {
-  //	Write the code for releasing read-write lock by the writer.
-}
+void WriterUnlock(struct read_write_lock *rw) { sem_post(&rw->write_lock); }
--- a4/rwlock.h
+++ a4/rwlock.h
@@ -1,9 +1,14 @@
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-struct read_write_lock {};
+struct read_write_lock {
+  sem_t resource_lock; // lock for atomically updating resources of this struct
+  sem_t write_lock;    // lock which writers must have to write
+  int num_readers;     // Number of readers currently reading the resource
+};
 
 void InitalizeReadWriteLock(struct read_write_lock *rw);
 void ReaderLock(struct read_write_lock *rw);
